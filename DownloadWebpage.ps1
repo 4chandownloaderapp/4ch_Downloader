d402f7bf4337ced67d85318ab1e1b6088467e1f4
Piotr <4chan_downloader_app@superbox.pl>
2021-10-25 17:55:47 +0200

    eliminate DownloadWebpage.ps1 from project

--- DownloadWebpage.ps1
+++
@@ -1,6 +0,0 @@
-param(
-[string]$webpage
-)
-$WebClient=New-Object net.webclient;
-$WebClient.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0";
-$webClient.DownloadFile("$webpage", "D:\SSIS_Projects\4ch_Downloader\4ch_Downloader_Workspace\WebPage\Webpage.txt");
